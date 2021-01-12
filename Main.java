b368fff20c04667e172224fa7f3e87b0cd67a178
subalghimire <subal.ghimire2396@gmail.com>
2021-01-12 22:06:30 +0545

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Random;
+public class Main
+{
+	public static void main(String[] args) {
+       
+Random random = new Random();
+        int number = random.nextInt(1000);
+        System.out.println(number);
+	}
+}
